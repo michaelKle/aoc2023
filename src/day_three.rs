7d34994c9dc96fb8411c1dbc99a6a2d0ea924c39
michaelKle <3406283+michaelKle@users.noreply.github.com>
2024-11-21 09:43:23 +0100

    Convert to hashmap and regex

--- src/day_three.rs
+++ src/day_three.rs
@@ -1,4 +1,27 @@
-use std::{collections::HashSet, fs::read_to_string};
+extern crate regex;
+use regex::bytes::Regex;
+use std::{
+    cmp::{max, min},
+    collections::{HashMap, HashSet},
+    fs::read_to_string,
+};
+
+#[derive(Hash, Eq, PartialEq, Debug)]
+struct Key {
+    start: usize,
+    end: usize,
+    line: usize,
+}
+impl Key {
+    /// Creates a new Viking.
+    fn new(start: usize, end: usize, line: usize) -> Key {
+        Key {
+            start: start,
+            end: end,
+            line: line,
+        }
+    }
+}
 
 fn is_symbol(x: u8) -> bool {
     let non_symbols = "0123456789.";
@@ -10,6 +33,58 @@ fn is_number(x: u8) -> bool {
     let numbers = "0123456789";
 
     numbers.find(x as char) != None
+}
+
+fn get_num_in_ascii_slice(bytes: &[u8]) -> usize {
+    let mut erg: usize = 0;
+    let mut exp: usize = 1;
+    for x in (0..bytes.len()).rev() {
+        erg += (bytes[x] - '0' as u8) as usize * exp;
+        exp *= 10;
+    }
+    erg
+}
+
+fn numbers_in_line(bytes: &[u8], line: usize) -> HashMap<Key, usize> {
+    let mut ret = HashMap::new();
+
+    let re = Regex::new(r"(\d+)").unwrap();
+    for cap in re.find_iter(bytes) {
+        ret.insert(
+            Key::new(cap.start(), cap.end(), line),
+            get_num_in_ascii_slice(cap.as_bytes()),
+        );
+    }
+
+    ret
+}
+
+// checks for symbol and handles out ouf bounds gracefully
+// end is one beyond the last byte to be checked
+fn line_has_symbol(line: &[u8], start: usize, end: usize) -> bool {
+    let end = min(end, line.len());
+    for x in start..end {
+        let c = line[x];
+        if is_symbol(c) {
+            return true;
+        }
+    }
+    false
+}
+
+// checks for symbol around the position
+// end is one beyond the last byte to be checked
+fn symbol_around(
+    prev_line: &[u8],
+    current_line: &[u8],
+    next_line: &[u8],
+    start: usize,
+    end: usize,
+) -> bool {
+    let start = max(start as isize - 1, 0) as usize;
+    line_has_symbol(prev_line, start, end + 1)
+        || line_has_symbol(current_line, start, end + 1)
+        || line_has_symbol(next_line, start, end + 1)
 }
 
 // returns true if line has a character
@@ -124,6 +199,8 @@ pub fn sum_power_of_all_parts(filename: &str) -> u64 {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+
     use super::*;
 
     #[test]
@@ -139,12 +216,54 @@ mod tests {
     }
 
     #[test]
-    fn test_check_for_symbol_in_line() {
-        assert_eq!(has_symbol("...*......", 3), true);
-        assert_eq!(has_symbol("...*......", 2), true);
-        assert_eq!(has_symbol("...*......", 4), true);
-        assert_eq!(has_symbol("...*......", 0), false);
-        assert_eq!(has_symbol("...*......", 9), false);
+    fn test_returns_numbers_in_line() {
+        let expect1 = HashMap::from([(Key::new(0, 3, 3), 467), (Key::new(6, 8, 3), 14)]);
+        assert_eq!(numbers_in_line("467...14..".as_bytes(), 3), expect1);
+
+        let expect2 = HashMap::from([(Key::new(3, 7, 77), 4633), (Key::new(9, 11, 77), 55)]);
+        assert_eq!(numbers_in_line("..*4633..55".as_bytes(), 77), expect2);
+    }
+
+    #[test]
+    fn test_check_symbol_in_line() {
+        assert_eq!(line_has_symbol("...*......".as_bytes(), 1, 4), true);
+        assert_eq!(line_has_symbol("...*......".as_bytes(), 0, 2), false);
+        assert_eq!(line_has_symbol("...*......".as_bytes(), 3, 4), true);
+        assert_eq!(line_has_symbol("...*......".as_bytes(), 4, 3), false);
+    }
+
+    #[test]
+    fn test_check_for_symbol_in_lines() {
+        assert_eq!(
+            symbol_around(
+                "".as_bytes(),
+                "467..114..".as_bytes(),
+                "....*.....".as_bytes(),
+                0,
+                3
+            ),
+            false
+        );
+        assert_eq!(
+            symbol_around(
+                "".as_bytes(),
+                "467..114..".as_bytes(),
+                "....*.....".as_bytes(),
+                5,
+                8
+            ),
+            true
+        );
+        assert_eq!(
+            symbol_around(
+                "".as_bytes(),
+                "467*.114..".as_bytes(),
+                "....*.....".as_bytes(),
+                0,
+                3
+            ),
+            true
+        );
     }
 
     #[test]
